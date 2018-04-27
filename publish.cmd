394880a4d00433ce8b7ed6fe79935b4036e9bafd
Jim Lewallen <jameslew@microsoft.com>
2018-04-27 15:23:19 -0700

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=piglatinbot-sdkv4-dotnet-Web-Deploy.pubxml -p:Password=36xmmTd2PRud1xSomDnxmw5L6hvHig45PeA2dkoreugd5jRwclRYYCinXf79
+
